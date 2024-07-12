40e805a627db032f0403022e37f108abfdbc3242
Magnus Sillén <magnuskenneth@gmail.com>
2024-07-12 10:54:03 +0200

    Install mardown-preview plugin
    I mapped <leader>+mp to toggle markdown preview

---
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,11 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function()
+    vim.fn["mkdp#util#install"]()
+  end,
+  config = function()
+    vim.api.nvim_set_keymap("n", "<leader>mp", ":MarkdownPreviewToggle<CR>", { noremap = true, silent = true })
+  end,
+}
