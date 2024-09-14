2064e86aba3e5b981397c43921217f229ca51402
Magnus Sillén <magnuskenneth@gmail.com>
2024-09-14 09:08:14 +0200

    Add support form comments in jsx/tsx files

--- nvim/.config/nvim/lua/plugins/comment.nvim.lua
+++ nvim/.config/nvim/lua/plugins/comment.nvim.lua
@@ -1,6 +1,14 @@
 return {
-  "numToStr/Comment.nvim",
-  opts = {
-    -- add any options here
-  },
+	{
+		"numToStr/Comment.nvim",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
+	},
 }
