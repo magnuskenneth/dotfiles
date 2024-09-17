46aab3ab3cee102894671a2a0390ceabed6fd5bb
Magnus Sillén <magnuskenneth@gmail.com>
2024-09-17 20:29:13 +0200

    Show error messages in floating window

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,63 +1,73 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"tsserver",
-					"html",
-					"eslint",
-					"terraformls",
-					"svelte",
-					"jdtls",
-					"intelephense",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilites = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "html",
+          "eslint",
+          "terraformls",
+          "svelte",
+          "jdtls",
+          "intelephense",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilites = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilites,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilites,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilites,
-			})
-			lspconfig.eslint.setup({
-				capabilities = capabilites,
-			})
-			lspconfig.terraformls.setup({
-				capabilities = capabilites,
-			})
-			lspconfig.svelte.setup({
-				capabilities = capabilites,
-			})
-			lspconfig.jdtls.setup({
-				capabilities = capabilites,
-			})
-			lspconfig.intelephense.setup({
-				capabilities = capabilites,
-			})
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilites,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilites,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilites,
+      })
+      lspconfig.eslint.setup({
+        capabilities = capabilites,
+      })
+      lspconfig.terraformls.setup({
+        capabilities = capabilites,
+      })
+      lspconfig.svelte.setup({
+        capabilities = capabilites,
+      })
+      lspconfig.jdtls.setup({
+        capabilities = capabilites,
+      })
+      lspconfig.intelephense.setup({
+        capabilities = capabilites,
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-		end,
-	},
+      -- You will likely want to reduce updatetime which affects CursorHold
+      -- note: this setting is global and should be set only once
+      vim.o.updatetime = 250
+      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+        callback = function()
+          vim.diagnostic.open_float(nil, { focus = false })
+        end,
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+    end,
+  },
 }
