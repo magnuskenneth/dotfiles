cac752f3593403f834c14c3d51b85b7f54babcd5
Magnus Sillén <magnuskenneth@gmail.com>
2024-12-07 23:07:18 +0100

    Update fzf config

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,22 +1,24 @@
 return {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local fzf = require("fzf-lua")
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local fzf = require("fzf-lua")
 
-    fzf.setup({
-      files = {
-        actions = {
-          ["ctrl-q"] = { fn = fzf.actions.file_sel_to_qf, prefix = "select-all" },
-        },
-      },
-    })
+		fzf.setup({
+			files = {
+				actions = {
+					["ctrl-q"] = { fn = fzf.actions.file_sel_to_qf, prefix = "select-all" },
+				},
+			},
+		})
 
-    vim.keymap.set("n", "<leader>ff", fzf.files, {})
-    vim.keymap.set("n", "<leader>fb", fzf.buffers, {})
-    vim.keymap.set("n", "<leader>fh", fzf.help_tags, {})
-    vim.keymap.set("n", "<leader>fg", fzf.live_grep, {})
-    vim.keymap.set("n", "<leader>gs", fzf.git_status, {})
-  end,
+		vim.keymap.set("n", "<leader>ff", fzf.files, {})
+		vim.keymap.set("n", "<leader>fb", fzf.buffers, {})
+		vim.keymap.set("n", "<leader>fo", fzf.oldfiles, {})
+		vim.keymap.set("n", "<leader>fh", fzf.help_tags, {})
+		vim.keymap.set("n", "<leader>fg", fzf.live_grep, {})
+		vim.keymap.set("n", "<leader>gs", fzf.git_status, {})
+		vim.keymap.set("n", "<leader>gc", fzf.git_commits, {})
+	end,
 }
