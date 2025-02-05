2728a88d3b091073c9a2b161b36c0f0f0be20ef8
Magnus Sillén <magnuskenneth@gmail.com>
2025-02-05 22:04:53 +0100

    Update config and keymaps for copilot chat

--- nvim/.config/nvim/lua/plugins/copilot-chat.lua
+++ nvim/.config/nvim/lua/plugins/copilot-chat.lua
@@ -1,24 +1,46 @@
 return {
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		dependencies = {
-			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-		},
-		build = "make tiktoken", -- Only on MacOS or Linux
-		opts = {
-			-- See Configuration section for options
-		},
-		keys = {
-			-- Show prompts actions with fzf-lua
-			{
-				"<leader>ccp",
-				function()
-					local actions = require("CopilotChat.actions")
-					require("CopilotChat.integrations.fzflua").pick(actions.prompt_actions())
-				end,
-				desc = "CopilotChat - Prompt actions",
-			},
-		},
-	},
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    config = function()
+      require("CopilotChat").setup({
+        mappings = {
+          reset = {
+            normal = "<leader>cr",
+            detail = "Reset Chat",
+          },
+        },
+      })
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.fzflua").pick(actions.prompt_actions())
+        end,
+        desc = "CopilotChat - Prompt actions",
+      },
+      {
+        "<leader>cq",
+        function()
+          local input = vim.fn.input("quick chat: ")
+          if input ~= "" then
+            require("CopilotChat").ask(input, { selection = require("copilotchat.select").buffer })
+          end
+        end,
+        desc = "copilotchat - quick chat",
+      },
+      {
+        "<leader>ct",
+        function()
+          require("CopilotChat").toggle()
+        end,
+        desc = "CopilotChat - Toggle Chat",
+      },
+    },
+  },
 }
