6393bd478c6909de5eddedcc232282f75ccb83af
Magnus Sillén <magnuskenneth@gmail.com>
2025-02-18 16:23:41 +0100

    Update copilot conf and update lazy packages

--- nvim/.config/nvim/lua/plugins/copilot-chat.lua
+++ nvim/.config/nvim/lua/plugins/copilot-chat.lua
@@ -1,46 +1,46 @@
 return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" },
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-    },
-    config = function()
-      require("CopilotChat").setup({
-        mappings = {
-          reset = {
-            normal = "<leader>cr",
-            detail = "Reset Chat",
-          },
-        },
-      })
-    end,
-    keys = {
-      {
-        "<leader>cp",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.fzflua").pick(actions.prompt_actions())
-        end,
-        desc = "CopilotChat - Prompt actions",
-      },
-      {
-        "<leader>cq",
-        function()
-          local input = vim.fn.input("quick chat: ")
-          if input ~= "" then
-            require("CopilotChat").ask(input, { selection = require("copilotchat.select").buffer })
-          end
-        end,
-        desc = "copilotchat - quick chat",
-      },
-      {
-        "<leader>ct",
-        function()
-          require("CopilotChat").toggle()
-        end,
-        desc = "CopilotChat - Toggle Chat",
-      },
-    },
-  },
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" },
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		config = function()
+			require("CopilotChat").setup({
+				mappings = {
+					reset = {
+						normal = "<leader>cr",
+						detail = "Reset Chat",
+					},
+				},
+			})
+		end,
+		keys = {
+			{
+				"<leader>cp",
+				function()
+					local actions = require("CopilotChat.actions")
+					require("CopilotChat.integrations.fzflua").pick(actions.prompt_actions())
+				end,
+				desc = "CopilotChat - Prompt actions",
+			},
+			{
+				"<leader>cq",
+				function()
+					local input = vim.fn.input("quick chat: ")
+					if input ~= "" then
+						require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+					end
+				end,
+				desc = "copilotchat - quick chat",
+			},
+			{
+				"<leader>ct",
+				function()
+					require("CopilotChat").toggle()
+				end,
+				desc = "CopilotChat - Toggle Chat",
+			},
+		},
+	},
 }
--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,23 +1,24 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "InsertEnter",
-	config = function()
-		require("copilot").setup({
-			panel = {
-				auto_refresh = true,
-			},
-			suggestion = {
-				auto_trigger = true,
-				keymap = {
-					accept = "<Tab>",
-					next = "<C-n>",
-					prev = "<C-p>",
-					accept_word = "<C-w>",
-					accept_line = "<C-l>",
-				},
-			},
-			["*"] = true, -- If you add "*" as a filetype, the default configuration for filetypes won't be used anymore. e.g.
-		})
-	end,
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      panel = {
+        auto_refresh = true,
+      },
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          -- accept = "<Tab>",
+          accept = "<C-j>",
+          next = "<C-n>",
+          prev = "<C-p>",
+          accept_word = "<C-w>",
+          accept_line = "<C-l>",
+        },
+      },
+      ["*"] = true, -- If you add "*" as a filetype, the default configuration for filetypes won't be used anymore. e.g.
+    })
+  end,
 }
