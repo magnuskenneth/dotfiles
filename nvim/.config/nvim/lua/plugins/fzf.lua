b1acdaaec9ae3cb55a0476b4ebf067e297328e91
Magnus Sillén <magnuskenneth@gmail.com>
2025-06-23 15:16:41 +0200

    Use fzf floating windows for lsp stuff

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -6,6 +6,7 @@ return {
 		local fzf = require("fzf-lua")
 
 		fzf.setup({
+			fzf_bin = nil,
 			files = {
 				actions = {
 					["ctrl-q"] = { fn = fzf.actions.file_sel_to_qf, prefix = "select-all" },
@@ -20,5 +21,16 @@ return {
 		vim.keymap.set("n", "<leader>fg", fzf.live_grep, {})
 		vim.keymap.set("n", "<leader>gs", fzf.git_status, {})
 		vim.keymap.set("n", "<leader>gc", fzf.git_commits, {})
+
+		-- LSP keymaps using fzf-lua
+		vim.keymap.set("n", "<leader>ca", fzf.lsp_code_actions, { desc = "Code actions" })
+		vim.keymap.set("n", "<leader>cr", fzf.lsp_references, { desc = "References" })
+		vim.keymap.set("n", "<leader>ci", fzf.lsp_implementations, { desc = "Implementations" })
+		vim.keymap.set("n", "<leader>ct", fzf.lsp_typedefs, { desc = "Type definitions" })
+		vim.keymap.set("n", "<leader>cs", fzf.lsp_document_symbols, { desc = "Document symbols" })
+		vim.keymap.set("n", "<leader>cS", fzf.lsp_workspace_symbols, { desc = "Workspace symbols" })
+		vim.keymap.set("n", "gd", fzf.lsp_definitions, { desc = "Go to definition" })
+		vim.keymap.set("n", "gr", fzf.lsp_references, { desc = "Go to references" })
+		vim.keymap.set("n", "gi", fzf.lsp_implementations, { desc = "Go to implementation" })
 	end,
 }
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,78 +1,78 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        automatic_enable = true,
-        ensure_installed = {
-          "lua_ls",
-          "ts_ls",
-          "html",
-          "tailwindcss",
-          "eslint",
-          "terraformls",
-          "svelte",
-          "jdtls",
-          "intelephense",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilites = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				automatic_enable = true,
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+					"html",
+					"tailwindcss",
+					"eslint",
+					"terraformls",
+					"svelte",
+					"jdtls",
+					"intelephense",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilites = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilites,
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilites,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilites,
-      })
-      lspconfig.tailwindcss.setup({
-        capabilities = capabilites,
-      })
-      lspconfig.eslint.setup({
-        capabilities = capabilites,
-      })
-      lspconfig.terraformls.setup({
-        capabilities = capabilites,
-      })
-      lspconfig.svelte.setup({
-        capabilities = capabilites,
-      })
-      lspconfig.jdtls.setup({
-        capabilities = capabilites,
-      })
-      lspconfig.intelephense.setup({
-        capabilities = capabilites,
-      })
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.eslint.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.terraformls.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.svelte.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.jdtls.setup({
+				capabilities = capabilites,
+			})
+			lspconfig.intelephense.setup({
+				capabilities = capabilites,
+			})
 
-      -- You will likely want to reduce updatetime which affects CursorHold
-      -- note: this setting is global and should be set only once
-      vim.o.updatetime = 250
-      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-        group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
-        callback = function()
-          vim.diagnostic.open_float(nil, { focus = false })
-        end,
-      })
+			-- You will likely want to reduce updatetime which affects CursorHold
+			-- note: this setting is global and should be set only once
+			vim.o.updatetime = 250
+			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+				group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+				callback = function()
+					vim.diagnostic.open_float(nil, { focus = false })
+				end,
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-    end,
-  },
+			-- vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			-- vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			-- vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			-- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+		end,
+	},
 }
