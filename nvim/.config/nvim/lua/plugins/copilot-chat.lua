5c728760c3fe9e8317d7610b2ed622971d466be4
Magnus Sillén <magnuskenneth@gmail.com>
2025-06-23 13:15:26 +0200

    Update copilot chat settings and keybindings

--- nvim/.config/nvim/lua/plugins/copilot-chat.lua
+++ nvim/.config/nvim/lua/plugins/copilot-chat.lua
@@ -1,47 +1,50 @@
 return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" },
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-    },
-    config = function()
-      require("CopilotChat").setup({
-        model = "claude-3.7-sonnet", -- Set Claude 3.7 Sonnet as the model
-        mappings = {
-          reset = {
-            normal = "<leader>cr",
-            detail = "Reset Chat",
-          },
-        },
-      })
-    end,
-    keys = {
-      {
-        "<leader>cp",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.fzflua").pick(actions.prompt_actions())
-        end,
-        desc = "CopilotChat - Prompt actions",
-      },
-      {
-        "<leader>cq",
-        function()
-          local input = vim.fn.input("quick chat: ")
-          if input ~= "" then
-            require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
-          end
-        end,
-        desc = "copilotchat - quick chat",
-      },
-      {
-        "<leader>ct",
-        function()
-          require("CopilotChat").toggle()
-        end,
-        desc = "CopilotChat - Toggle Chat",
-      },
-    },
-  },
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" },
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		config = function()
+			require("CopilotChat").setup({
+				model = "claude-3.7-sonnet", -- Set Claude 3.7 Sonnet as the model
+				mappings = {
+					reset = {
+						normal = "<leader>cr",
+						detail = "Reset Chat",
+					},
+				},
+			})
+		end,
+		keys = {
+			{
+				"<leader>zc",
+				mode = { "n", "v" },
+				function()
+					local actions = require("CopilotChat.actions")
+					require("CopilotChat.integrations.fzflua").pick(actions.prompt_actions())
+				end,
+				desc = "CopilotChat - Prompt actions",
+			},
+			{
+				"<leader>zq",
+				mode = { "n", "v" },
+				function()
+					local input = vim.fn.input("quick chat: ")
+					if input ~= "" then
+						require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+					end
+				end,
+				desc = "copilotchat - quick chat",
+			},
+			{
+				"<leader>zt",
+				mode = { "n", "v" },
+				function()
+					require("CopilotChat").toggle()
+				end,
+				desc = "CopilotChat - Toggle Chat",
+			},
+		},
+	},
 }
