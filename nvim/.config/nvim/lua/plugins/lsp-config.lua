80468972c95ad65f4351353c9ff77f1d85e20ddd
Magnus Sillén <magnuskenneth@gmail.com>
2025-06-27 10:04:24 +0200

    Enable leader+rn for rename
    I accidently removed it when updating to use fzf for other actions

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -72,7 +72,7 @@ return {
 			-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			-- vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
 			-- vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			-- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 		end,
 	},
 }
